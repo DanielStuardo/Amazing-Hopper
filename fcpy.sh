c02642a5853857c5ad9ee6bdf9eefcd19a85581a
Mr. Dalien <daniel.stuardo@gmail.com>
2023-01-06 00:09:50 -0300

    Add files via upload

---
+++ fcpy.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+sudo cp include/mistral.h /usr/include/hopper
+#sudo cp include/poirot.h /usr/include/hopper
+sudo cp include/flow.h /usr/include/hopper
+sudo cp include/flow-flow.h /usr/include/hopper
+sudo cp include/flow-regexp.h /usr/include/hopper
+sudo cp include/flow-term.h /usr/include/hopper
+sudo cp include/keys.h /usr/include/hopper
+sudo cp include/flow-input.h /usr/include/hopper
+sudo cp include/jambo.h /usr/include/hopper
+sudo cp include/hbasic.h /usr/include/hopper
+exit 0
